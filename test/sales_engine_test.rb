ea1eac90b1cb9497688077f0bb282128fdb3dd9f
Jeffrey Ruane <ruane.jb@gmail.com>
2015-08-05 13:24:47 -0600

    test_it_can_instantiate_new_depositories

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,4 +3,15 @@ require 'minitest/pride'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
+  def test_it_can_instantiate_new_depositories
+    engine = SalesEngine.new
+    engine.startup
+
+    assert engine.customer_repository.kind_of?(CustomerRepository)
+    assert engine.invoice_item_repository.kind_of?(InvoiceItemRepository)
+    assert engine.invoice_repository.kind_of?(InvoiceRepository)
+    assert engine.item_repository.kind_of?(ItemRepository)
+    assert engine.merchant_repository.kind_of?(MerchantRepository)
+    assert engine.transaction_repository.kind_of?(TransactionRepository)
+  end
 end
